9844a68b0d6bfb7146bcbdbd0974b93cbdc9fc95
Verissimo Casas <verissimo@executive.com>
2023-11-08 00:49:20 -0400

    new feature

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import '../style.css'
-import { setupCounter } from './counter.js'
+import '../Alpinecdn.min.js'
 
 document.querySelector('#app').innerHTML = `
-
-    <img class="mx-auto" src="/assets/98617.svg" width="700" height="128">
+<div x-data="{ showFirstImage: true }">
+    <img class="mx-auto" src="/assets/stop.svg" width="700" height="128" x-show="showFirstImage" x-effect="console.log('Showing Image 1')">
+    <img class="mx-auto" src="/assets/Passarela.svg" width="700" height="128" x-show="!showFirstImage" x-effect="console.log('Showing Image 2')">
+    <button @click="showFirstImage = !showFirstImage">Toggle Image</button>
+</div>
 
 `
+// <img class="mx-auto" src="/assets/98617.svg" width="700" height="128">
 // <img class="mx-auto" src="/assets/Passarela.svg" width="700" height="128">
-
-setupCounter(document.querySelector('#counter'))
