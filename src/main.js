7768fc42c1417b4ae1b84eb1abbdb16873808c21
Verissimo Casas <Verissimo@executive.com>
2023-11-08 14:23:22 -0400

    mouse scroll altera evento

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
-import '../style.css'
-import '../Alpinecdn.min.js'
+import "../style.css";
+import "../Alpinecdn.min.js";
 
-document.querySelector('#app').innerHTML = `
-<div x-data="{ showFirstImage: true }">
-    <img class="mx-auto" src="/assets/stop.svg" width="700" height="128" x-show="showFirstImage" x-effect="console.log('Showing Image 1')">
-    <img class="mx-auto" src="/assets/Passarela.svg" width="700" height="128" x-show="!showFirstImage" x-effect="console.log('Showing Image 2')">
-    <button @click="showFirstImage = !showFirstImage">Toggle Image</button>
+document.querySelector("#app").innerHTML = `
+<div x-data="{ showFirstImage: true }" x-on:wheel="${onMouseScroll}($event)">
+    <img class="mx-auto" src="/assets/stop.svg" width="700" height="128" x-show="showFirstImage">
+    <img class="mx-auto" src="/assets/Passarela.svg" width="700" height="128" x-show="!showFirstImage">
 </div>
+`;
 
-`
+function onMouseScroll(event) {
+    if (event.deltaY > 0) {
+        showFirstImage = !showFirstImage;
+        console.log('Toggled image using mouse scroll');
+    }
+}
+
+
 // <img class="mx-auto" src="/assets/98617.svg" width="700" height="128">
 // <img class="mx-auto" src="/assets/Passarela.svg" width="700" height="128">
