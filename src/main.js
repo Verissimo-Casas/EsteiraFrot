e3a2c1a096cf7b344770ed1e9f7e24797c23e44f
Verissimo Casas <verissimo@executive.com>
2023-11-07 23:42:14 -0400

    Image Update

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
 
-// <img class="mx-auto" src="/assets/stop.svg" width="700" height="128">
+    <img class="mx-auto" src="/assets/98617.svg" width="700" height="128">
+
 `
 // <img class="mx-auto" src="/assets/Passarela.svg" width="700" height="128">
 
